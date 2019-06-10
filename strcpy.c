554b9da6fec6eb10d431479477a292337539da74
qingdujun <qdujun@gmail.com>
2019-06-10 20:36:57 +0800

    add 252/256/266/270

---
+++ strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <assert.h>
+
+char* strcpy2(char* str_dest, char const* str_src) {
+    assert(str_dest != NULL && str_src != NULL);
+
+    char* address = str_dest;
+    while ((*str_dest++ = *str_src++) != '\0')
+        ;
+
+    return address;
+}
+
+int main(int argc, char const *argv[]) {
+    char* src = "hello strcpy";
+
+    char buffer[100];
+
+    strcpy2(buffer, src);
+
+    printf("%s\n", buffer);
+
+    return 0;
+}
